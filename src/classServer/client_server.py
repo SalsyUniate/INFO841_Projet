dfede34af52aa90fcb4d1b74f96d4b3a69d7d2b8
Cloé Léglise <cloe.leglise@free.fr>
2023-05-16 22:55:49 +0200

    improve docstrings

--- src/classServer/client_server.py
+++ src/classServer/client_server.py
@@ -1,5 +1,5 @@
 """
-# TODO: write documentation
+Implementing client proxy
 """
 
 import signal
@@ -11,14 +11,25 @@ import rsa
 
 class ClientServer:
     """
-    TODO: write documentation
-
+    Implementing a client proxy server with RSA algorithm
     """
 
-    def __init__(self, port_in, port_out, port_rsa):
+
+    def __init__(self, socket_in, socket_out, socket_rsa):
+        """
+        Start client server : initialize secured connection and waiting for connection
+
+        Parameters :
+            socket_in (int) : port number of this server
+            socket_out (int) : port number of web proxy server
+            socket_rsa (int) : port number to share rsa keys
+
+        No returns
+        """
 
         print("Starting client proxy server...")
 
+        # can stop the server with Ctl+C in shell
         self.shutdown = 0
         signal.signal(signal.SIGINT, self.shutdown)
 
@@ -27,40 +38,45 @@ class ClientServer:
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         # binding socket to host and port
-        self.server_socket.bind(('localhost', port_in))
+        self.server_socket.bind(('localhost', socket_in))
 
         # waiting queue for requests
         self.server_socket.listen(10)
 
-        self.secure_connection(port_rsa)
+        # initialize secured connection
+        self.secure_connection(socket_rsa)
 
         print("Server up !\n")
+
         id_thread = 1
         while True :
             (client_socket, _) = self.server_socket.accept()
             # making thread correspondig to the request
             thread = threading.Thread(target=self.proxy_thread,
                                       args = (client_socket,
-                                              port_out,
+                                              socket_out,
                                               id_thread))
-
             id_thread += 1
             thread.daemon = True
             thread.start()
 
 
-
-    def secure_connection(self, port_rsa):
+    def secure_connection(self, socket_rsa):
         """
         Initialize the rsa key to scure the connection
-        Args:
-            port_rsa (int): port numver to connect
+
+        Parameters :
+            socket_rsa (int): socket number to connect
+
+        No returns
         """
+
         print("Initializing secured connection...")
+
         # generating encryption keys
         (self.public_key, self.private_key) = rsa.newkeys(512)
 
-
+        # get public key in bytes to send it
         str_public_key = str(self.public_key)[10:-1].split(", ")
         bytes_public_key = [str.encode(str_public_key[0]),
                             str.encode(str_public_key[1])]
@@ -68,33 +84,38 @@ class ClientServer:
 
         # binding socket to host and port
         self.rsa_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.rsa_socket.connect(('localhost', port_rsa))
+        self.rsa_socket.connect(('localhost', socket_rsa))
 
+        # send public key to web proxy
         sleep(1)
         self.rsa_socket.send(bytes_public_key[0])
         sleep(1)
         self.rsa_socket.send(bytes_public_key[1])
+
+        # receiving web proxy public key
         bytes_out_key = [self.rsa_socket.recv(4096)]
         bytes_out_key.append(self.rsa_socket.recv(4096))
 
+        # convert web proxy public key in rsa.PublicKey
         int_out_key = [int(str(bytes_out_key[0])[2:-1]),
                     int(str(bytes_out_key[1])[2:-1])]
         self.web_proxy_key = rsa.PublicKey(int_out_key[0], int_out_key[1])
 
+        # closing rsa socket
         self.rsa_socket.close()
 
-    def proxy_thread(self, client_socket, port_out, id_thread):
+
+    def proxy_thread(self, client_socket, socket_out, id_thread):
         """
         Handle a request from the client
 
-        Args:
-            client_socket (_type_): _description_
-            port_out (_type_): _description_
-            id_thread (_type_): _description_
-            server_keys (_type_): _description_
-            web_proxy_key (_type_): _description_
-        """
+        Parameters :
+            client_socket (socket): socket to communicate with the client
+            socket_out (int): socket number of the web proxy
+            id_thread (int): the id of the thread
 
+        No returns
+        """
 
         print(f"New connection (id : {id_thread})!")
         request = str(client_socket.recv(4096))
@@ -103,37 +124,39 @@ class ClientServer:
         # parse the first line
         first_line = request.split('\n', maxsplit=1)[0]
         url = first_line.split(' ')[1].replace('http://', '')[:-1]
-        # print(url)
 
-
-        ### creation of the thread socket
+        # creation of the thread socket
         thread_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         thread_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        thread_socket.connect(('localhost', port_out))
+        thread_socket.connect(('localhost', socket_out))
 
+        # encrypt request and send it to the web proxy
         masqued_url = rsa.encrypt(url.encode('ascii'), self.web_proxy_key)
         thread_socket.send(masqued_url)
         print(f"  Thread {id_thread}: Request sent to web proxy")
 
+        # receiving response from web proxy
         masqued_result = []
         end_bool = False
         while not end_bool:
             new = thread_socket.recv(4096)
             if new == b'END':
+                # stop listening when receiving 'END'
                 end_bool = True
             else : masqued_result.append(new)
 
-
+        # decrypt response from web proxy
         clear_result = ''
         for part in enumerate(masqued_result):
             clear_result += str(rsa.decrypt(masqued_result[part[0]], self.private_key))[2:-1]
 
+        # remove '\\' in the response
         clear_result = clear_result.replace("\\n", "\n")
         clear_result = clear_result.replace("\\r", "\r")
 
         # sending answer to client
         client_socket.send(bytes(clear_result, 'utf-8'))
 
-
+        # ending thread
         print(f"  Thread {id_thread}: Response sent to client")
         print(f"Closing thread {id_thread}")
--- src/classServer/web_server.py
+++ src/classServer/web_server.py
@@ -1,5 +1,5 @@
 """
-# TODO: write documentation
+Implementing web proxy
 """
 
 import signal
@@ -9,16 +9,27 @@ from time import sleep
 import rsa
 
 
-
 class WebServer :
     """
-    # TODO: xrite docstring
+    Implementing a web proxy server with RSA algorithm
     """
+
+
     def __init__(self, port_out, port_web, port_rsa):
+        """
+        Start web server : initialize secured connection and waiting for connection
+
+        Parameters :
+            socket_out (int) : port number of this server
+            socket_web (int) : port number of web servers
+            socket_rsa (int) : port number to share rsa keys
+
+        No returns
+        """
 
         print("Starting web proxy server...")
 
-
+        # can stop the server with Ctl+C in shell
         self.shutdown = 0
         signal.signal(signal.SIGINT, self.shutdown)
 
@@ -32,11 +43,11 @@ class WebServer :
         # waiting queue for requests
         self.server_socket.listen(10)
 
-
+        # initialize secured connection
         self.secure_connection(port_rsa)
 
-
         print("Server up !\n")
+
         id_thread = 1
         while True :
             (client_socket, _) = self.server_socket.accept()
@@ -49,12 +60,18 @@ class WebServer :
             thread.daemon = True
             thread.start()
 
+
     def secure_connection(self, port_rsa) :
         """
-        # TODO: write documentation
-        """
-        print("Initializing secured connection...")
+        Initialize the rsa key to scure the connection
 
+        Parameters :
+            socket_rsa (int): socket number to connect
+
+        No returns
+        """
+
+        print("Initializing secured connection...")
 
         # generating encryption keys
         self.public_key, self.private_key = rsa.newkeys(512)
@@ -63,67 +80,86 @@ class WebServer :
         self.rsa_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.rsa_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+        # binding socket and listening
         self.rsa_socket.bind(('localhost', port_rsa))
         self.rsa_socket.listen()
 
+        # accepting the connection
         (self.rsa_connected, _) = self.rsa_socket.accept()
 
-
+        # receiving client proxy publi key
         bytes_in_key = []
         while len(bytes_in_key) < 2 :
             bytes_in_key.append(self.rsa_connected.recv(4096))
 
+        # get public key in bytes to send it
         str_public_key = str(self.public_key)[10:-1].split(", ")
         bytes_public_key = [str.encode(str_public_key[0]),
                             str.encode(str_public_key[1])]
 
+        # send public key to client proxy
         self.rsa_connected.send(bytes_public_key[0])
         sleep(1)
         self.rsa_connected.send(bytes_public_key[1])
 
-
+        # convert web proxy public key in rsa.PublicKey
         int_in_key = [int(str(bytes_in_key[0])[2:-1]),
                     int(str(bytes_in_key[1])[2:-1])]
         self.client_proxy_key = rsa.PublicKey(int_in_key[0], int_in_key[1])
 
+        # closing rsa socket
         self.rsa_socket.close()
 
 
     def proxy_thread(self, client_socket, port_web, id_thread):
         """
-        # TODO: write documentation
+        Handle a request from the client
+
+        Parameters :
+            client_socket (socket): socket to communicate with the client proxy
+            socket_out (int): socket number of the web server
+            id_thread (int): the id of the thread
+
+        No returns
         """
+
         print(f"New connection (id : {id_thread})!")
         request = client_socket.recv(4096)
         print(f"  Thread {id_thread}: Request received")
+
+        # decrypt request from client proxy
         request = str(rsa.decrypt(request, self.private_key))
         request = request.replace("b'", "")
         request = request.replace("'", "")
 
-
-        ### creation of the thread socket
-
+        # creation of the thread socket
         thread_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         thread_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         thread_socket.connect((request, port_web))
+
+        # build a new request for web server and send it
         req = f"GET / HTTP/1.1\r\nHost:{request}\r\n\r\n"
         thread_socket.send(bytes(req, 'utf-8'))
         print(f"  Thread {id_thread}: Request sent to web server")
+
+        # get respinse from web server
         result = thread_socket.recv(4096)
 
-
+        # encrypt response from web server
         masqued_result = []
         for index in range(0, len(result), 53):
             part = result[index:index+53]
+            # doing by step because size is limited in rsa.encrypt
             masqued_result.append(rsa.encrypt(part, self.client_proxy_key))
 
-
-        # sending answer to client
+        # sending answer to client proxy
         for part in enumerate(masqued_result):
             client_socket.send(masqued_result[part[0]])
             sleep(0.0005)
 
+        # alert client proxy all the data where sent
         client_socket.send(b'END')
+
+        # ending thread
         print(f"  Thread {id_thread}: Response sent to client proxy")
         print(f"Closing thread {id_thread}")
-        
